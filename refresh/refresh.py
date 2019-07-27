10d1755d67bb18dbbfc02b8c73b8b9d1749d7b4e
taro <taro@datsec.com>
2019-07-27 12:27:56 +0800

    fix status

--- refresh/refresh.py
+++ refresh/refresh.py
@@ -12,6 +12,7 @@ def refresh_flag():
         problem.flag, command = generate_flag_command(problem.template.change_flag_command)
         docker_controller.exec_container(problem.container_id, command)
         problem.rounds = problem.rounds + 1
+        problem.status = 'running'
         problem.save()
         check_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         print(f"{check_time}  ROUNDS {{{problem.rounds}}} {problem.container_id} flag update {problem.flag}")
